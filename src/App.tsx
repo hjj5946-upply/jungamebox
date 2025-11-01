0c095ad72c564279a2e445ce925d9bead1685cca
hjj <hjj5946@gmail.com>
2025-11-01 16:36:05 +0900

    251101_2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
-import "./index.css"; // 반드시 있어야 Tailwind 적용됨
+import "./index.css";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/games/roulette" element={<RoulettePage />} />
-        <Route path="/games/balance" element={<BalanceGamePage />} />
-        <Route path="/games/random-question" element={<RandomQuestionPage />} /> */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
